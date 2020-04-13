6fab1b4a4869844811d6de8536e8393018ef30ee
Nihat <nihatallahverdiyev@gmail.com>
2020-04-13 21:59:37 +0200

    Fix MySql connectivity issue, modify styles.css

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -3,25 +3,25 @@ delete from Taco_Ingredients;
 delete from Taco;
 delete from Taco_Order;
 
-delete from Ingredient;
+delete from ingredient;
 */
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('FLTO', 'Flour Tortilla', 'WRAP');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('COTO', 'Corn Tortilla', 'WRAP');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('GRBF', 'Ground Beef', 'PROTEIN');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('CARN', 'Carnitas', 'PROTEIN');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('TMTO', 'Diced Tomatoes', 'VEGGIES');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('LETC', 'Lettuce', 'VEGGIES');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('CHED', 'Cheddar', 'CHEESE');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('JACK', 'Monterrey Jack', 'CHEESE');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('SLSA', 'Salsa', 'SAUCE');
-insert into INGREDIENT (ID, NAME, TYPE)
+insert into ingredient (id, name, type)
 values ('SRCR', 'Sour Cream', 'SAUCE');
